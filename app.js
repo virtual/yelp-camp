cc7279fa476335ba42ca66956a6d71f09a400111
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-08 19:00:49 -0600

    added new campground route and form

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 let express = require("express");
 let app = express();
-let bodyParser = require("express");
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.set("port", 5000);
+
+let campgrounds = [  
+  {
+    name: "Salmon Creek", 
+    img: "https://tinyurl.com/yae5xn8j"
+  },{
+    name: "Black Mountain", 
+    img: "https://tinyurl.com/y72fem6d"
+  },{
+    name: "Red Piny", 
+    img: "https://tinyurl.com/y8cujm2z"
+  },
+];
 
 app.get("/", function(req, res){
   res.render("landing")
@@ -10,20 +24,26 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
 
-  let campgrounds = [  
-    {
-      name: "Salmon Creek", 
-      img: "https://tinyurl.com/yae5xn8j"
-    },{
-      name: "Black Mountain", 
-      img: "https://tinyurl.com/y72fem6d"
-    },{
-      name: "Red Piny", 
-      img: "https://tinyurl.com/y8cujm2z"
-    },
-  ];
   res.render("campgrounds", {campgrounds: campgrounds});
-})
+});
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+app.post("/campgrounds/", function(req, res) {
+  let name = req.body.name;
+  let img = req.body.name;
+  let campground = {
+    name: name,
+    img: img
+  };
+  campgrounds.push(campground);
+  res.redirect("/campgrounds");
+});
+ 
+
+app.put("/campground/:id", function(req, res) {
+  
+  });
 
 app.listen(app.get("port"), function(){
   console.log("app listening on port", app.get("port"));
